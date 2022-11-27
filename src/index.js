14c2c5e7e72d90af3f14c50c2506722879cc49dd
Maksim Frankov <2m.franko@gmail.com>
2022-11-27 14:49:53 +0200

    first steps =)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+// fetchCountries(name)
+
+// fetch('https://restcountries.com/v3.1/name/peru')
+//     .then(function (res) {
+//         return res.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     })
+//     .catch(function (err) {
+//         console.log('error' + err);
+//     })
+
+// let countries;
+import countryCardTpl from './tamplates/country-card.hbs';
+fetch('https://restcountries.com/v3.1/name/peru')
+    .then(res => res.json())
+    .then(data => initialize(data))
+    .catch(err => console.log('Error' + err));
+
+function initialize(countriesData) {
+    const markup = countryCardTpl(countriesData[0]);
+    console.log(countriesData[0].languages);
+    console.log(markup);
+}
+    
