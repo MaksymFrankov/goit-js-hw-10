e35ae50225e4419ed31ade0969ecf23f556a7c0b
Maksim Frankov <2m.franko@gmail.com>
2022-11-29 00:27:14 +0200

    finall

--- src/index.js
+++ src/index.js
@@ -1,8 +1,78 @@
 import './css/styles.css';
 import debounce from "lodash.debounce";
+import countryCardTpl from './tamplates/country-card.hbs';
+import countryCardList from './tamplates/countrys-list.hbs';
+import Notiflix from 'notiflix';
 
+
+const refs = {
+    searchForm: document.querySelector('#search-box'),
+    countryContainer: document.querySelector('.country-info'),
+    countrysList: document.querySelector('.country-list')
+};
 
 const DEBOUNCE_DELAY = 300;
+
+
+refs.searchForm.addEventListener('input', debounce(onInput,DEBOUNCE_DELAY));
+
+function onInput(e) {
+    e.preventDefault();
+    // console.log(refs.searchForm.value);
+    if (refs.searchForm.value.trim()) {
+        fetchCountry(refs.searchForm.value.trim())
+            .then(renderCountryCard)
+            .catch(onFetchError);
+    }
+    else {
+        const markup = '';
+        refs.countryContainer.innerHTML = markup;
+    }
+}
+
+
+function fetchCountry(countryName) {
+    const url = `https://restcountries.com/v3.1/name/${countryName}`
+    return fetch(url)
+        .then(res => {
+            if (res.ok)
+            { return res.json() }
+            else
+            {
+                const markup = '';
+                refs.countryContainer.innerHTML = markup;
+                Notiflix.Notify.failure('Oops, there is no country with that name')
+            }
+        })
+    // .then(renderCountryCard)
+    // .catch(err => console.log('Error' + err)); 
+}
+
+function renderCountryCard(countriesData) {
+    if (countriesData.length >= 10)
+    {
+        const markup = '';
+        refs.countryContainer.innerHTML = markup;
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+    }
+    else if (countriesData.length < 10 && countriesData.length >= 2)
+    {
+        // console.log(countriesData)
+        markup = countryCardList(countriesData);
+        refs.countryContainer.innerHTML = markup;
+    }
+    else if (countriesData.length === 1)
+    {
+        const markup = countryCardTpl(countriesData[0]);
+        refs.countryContainer.innerHTML = markup;
+    }
+}
+
+function onFetchError(error) {
+    console.log('Somthing go wrong')
+}
+
+
 // fetchCountries(name)
 
 // fetch('https://restcountries.com/v3.1/name/peru')
@@ -18,48 +88,9 @@ const DEBOUNCE_DELAY = 300;
 
 // let countries;
 
-const refs = {
-    countryContainer: document.querySelector('.country-info'),
-    searchForm: document.querySelector('#search-box')
-};
-import countryCardTpl from './tamplates/country-card.hbs';
-
-refs.searchForm.addEventListener('input', debounce(onInput,DEBOUNCE_DELAY));
-
-function onInput(e) {
-    e.preventDefault();
-    // console.log(refs.searchForm.value);
-    fetchCountry(refs.searchForm.value.trim())
-    .then(renderCountryCard)
-    .catch(onFetchError);
-}
-
 // function initialize(countriesData) {
 //     const markup = countryCardTpl(countriesData[0]);
 //     console.log(countriesData[0].languages);
 //     refs.countryContainer.innerHTML = markup;
 //     console.log(markup);
 // }
-
-
-
-function fetchCountry(countryName) {
-    const url = `https://restcountries.com/v3.1/name/${countryName}`
-    return fetch(url)
-        .then(res => { if (res.ok) { return res.json() } else {alert('NERABOTAET POCHEMUTO')} })
-    // .then(renderCountryCard)
-    // .catch(err => console.log('Error' + err)); 
-}
-
-function renderCountryCard(countriesData) {
-    const markup = countryCardTpl(countriesData[0]);
-    refs.countryContainer.innerHTML = markup;
-    // console.log(countriesData[0].languages);
-    
-    // console.log(markup);
-}
-
-function onFetchError(error) {
-    alert('NERABOTAET POCHEMUTO')
-}
-    
