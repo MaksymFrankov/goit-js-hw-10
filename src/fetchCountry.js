7e298a1cfa5be5a7c6c0f99baa85588a72b28517
Maksim Frankov <2m.franko@gmail.com>
2022-12-01 14:17:57 +0200

    butify =)

--- src/fetchCountry.js
+++ src/fetchCountry.js
@@ -1,15 +1,10 @@
 export { fetchCountry };
 
-import Notiflix from 'notiflix';
-
 async function fetchCountry(countryName) {
     const url = `https://restcountries.com/v3.1/name/${countryName}`
     const res = await fetch(url);
-    if (res.ok) { return res.json(); }
-
-    else {
-        const markup = '';
-        document.querySelector('.country-info').innerHTML = markup;
-        Notiflix.Notify.failure('Oops, there is no country with that name');
-    }
+    // console.log(res);
+    if (res.status == 200) {return res.json();  }
+    else if (res.status == 404) { return 'Not found' }
+    else {return 'Unexpected error'}
 };
--- src/index.js
+++ src/index.js
@@ -8,60 +8,67 @@ import { fetchCountry } from './fetchCountry.js';
 const refs = {
     searchForm: document.querySelector('#search-box'),
     countryContainer: document.querySelector('.country-info'),
-    countrysList: document.querySelector('.country-list')
+    countrysList: document.querySelector('.country-list'),
 };
 
 const DEBOUNCE_DELAY = 300;
+const INFO_MESSAGE = 'Too many matches found. Please enter a more specific name.';
+const NO_COUNTRY_FOUND_MESSAGE = 'Oops, there is no country with that name';
+const ERROR_MESSAGE = 'Oops, somthing go wrong, please try later';
 
 
-refs.searchForm.addEventListener('input', debounce(onInput,DEBOUNCE_DELAY));
+refs.searchForm.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
     e.preventDefault();
     if (refs.searchForm.value.trim()) {
         fetchCountry(refs.searchForm.value.trim())
-            .then(renderCountryCard)
+            .then(onRespondCheck)
             .catch(onFetchError);
     }
     else {
-        const markup = '';
-        refs.countryContainer.innerHTML = markup;
+        clearMarkup();
     }
+};
+
+function onRespondCheck(countriesData) {
+    if (countriesData=== 'Not found') {
+        clearMarkup();
+        Notiflix.Notify.failure(NO_COUNTRY_FOUND_MESSAGE);
+    }
+    else if (countriesData === 'Unexpected error') {
+        Notiflix.Notify.failure(ERROR_MESSAGE);
+    }
+    else if (countriesData) {
+
+        if (countriesData.length >= 10) {
+            clearMarkup();
+            Notiflix.Notify.info(INFO_MESSAGE);
+        }
+
+        else if (countriesData.length < 10 && countriesData.length >= 2) {
+            renderCountriesList(countriesData);
+        }
+        
+        else if (countriesData.length === 1) {
+            renderCountryCard(countriesData);
+        }
+    } 
 }
 
-
-// function fetchCountry(countryName) {
-//     const url = `https://restcountries.com/v3.1/name/${countryName}`
-//     return fetch(url)
-//         .then(res => {
-//             if (res.ok)
-//             { return res.json() }
-//             else
-//             {
-//                 const markup = '';
-//                 refs.countryContainer.innerHTML = markup;
-//                 Notiflix.Notify.failure('Oops, there is no country with that name')
-//             }
-//         })
-// }
+function renderCountriesList(countriesData) {
+    refs.countryContainer.innerHTML = countryCardList(countriesData);
+};
 
 function renderCountryCard(countriesData) {
-    if (countriesData.length >= 10) {
-        const markup = '';
-        refs.countryContainer.innerHTML = markup;
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
-    }
-
-    else if (countriesData.length < 10 && countriesData.length >= 2) {
-        refs.countryContainer.innerHTML = countryCardList(countriesData);
-    }
-        
-    else if (countriesData.length === 1) {
-        const markupCountry = countryCardTpl(countriesData[0]);
-        refs.countryContainer.innerHTML = markupCountry;
-    }
+    refs.countryContainer.innerHTML = countryCardTpl(countriesData[0]);
 }
+
+function clearMarkup() {
+    refs.countrysList.innerHTML = '';
+    refs.countryContainer.innerHTML = '';
+};
 
 function onFetchError(error) {
-    console.log('Somthing go wrong' + " " + error)
-}
+    console.log(error);
+};
